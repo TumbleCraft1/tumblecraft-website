2174a0d0a624feda775c331e643a65b359815bdd
Ashank Sundaram <ashank@ashank.tech>
2025-07-17 12:15:43 +1000

    Comment out ServerInfo component in Home page for future revisions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@
 
 import Navigation from '@/components/Navigation'
 import HeroSection from '@/components/HeroSection'
-import ServerInfo from '@/components/ServerInfo'
+// import ServerInfo from '@/components/ServerInfo'
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Navigation />
       <HeroSection />
-      <ServerInfo />
+      {/* <ServerInfo /> */}
     </main>
   )
 }
